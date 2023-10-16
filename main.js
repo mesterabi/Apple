063bc17397d95c06d5bcb5637f8155281b6d5469
Mobina Esterabi <esterabimobina@gmail.com>
2023-10-16 16:54:24 -0400

    Wrapper swiper section completed.

---
+++ main.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: 9,
+  slidesToShow: 3,
+  slidesPerView: 1.2,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  autoplayHoverPause: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // breakpoints: {
+  //     576: {
+  //       slidesPerView: 1,
+  //       spaceBetween: 20,
+  //     },
+  //     768: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 40,
+  //     },
+  //     1200: {
+  //       slidesPerView: 3,
+  //       spaceBetween: 50,
+  //     },
+  //   },
+});
