2924b49e55c92271548aee0fb4173f09aa465d9e
Mobina Esterabi <esterabimobina@gmail.com>
2023-10-24 20:20:53 -0400

    footer directory collapse completed.

--- main.js
+++ main.js
@@ -4,40 +4,20 @@ var swiper = new Swiper(".mySwiper", {
   loop: 9,
   slidesToShow: 3,
   slidesPerView: 1.2,
-  // autoplay: {
-  //   delay: 3000,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
   autoplayHoverPause: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-
-  // breakpoints: {
-  //   576: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 20,
-  //   },
-  //   768: {
-  //     slidesPerView: 1.9,
-  //     spaceBetween: 5,
-  //     loop: 9,
-  //   },
-  // 1200: {
-  //   slidesPerView: 3,
-  //   spaceBetween: 50,
-  // },
-  // },
 });
 var swiper = new Swiper(".mySwiper-s", {
-  // spaceBetween: 15,
-  // centeredSlides: true,
-  // loop: 9,
-  // slidesToShow: 3,
-  // slidesPerView: 1.9,
-  slidesPerView: 3,
-  spaceBetween: 30,
+  slidesPerView: 1.9,
+  centeredSlides: "true",
+  spaceBetween: 10,
   loop: true,
   pagination: {
     el: ".swiper-pagination",
@@ -47,24 +27,23 @@ var swiper = new Swiper(".mySwiper-s", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
   // autoplay: {
   //   delay: 3000,
   //   disableOnInteraction: false,
   // },
-
-  // breakpoints: {
-  //   576: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 20,
-  //   },
-  //   768: {
-  //     slidesPerView: 1.9,
-  //     spaceBetween: 5,
-  //     loop: 9,
-  //   },
-  // 1200: {
-  //   slidesPerView: 3,
-  //   spaceBetween: 50,
-  // },
-  // },
 });
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
